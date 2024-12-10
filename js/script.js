0badf2c00454911053353921ac35f3010e7fa5a8
Ortello7 <domenicoortello00@gmail.com>
2024-12-10 18:55:18 +0100

    feat index

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@
 // -va applicato uno sconto del 20% per i minorenni
 // -va applicato uno sconto del 40% per gli over 65.
 
-// Iniziamo implementando il programma senza alcuna estetica:
-// usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
-// La risposta finale (o output) sarà anch’essa da scrivere in console.
+const name = document.getElementById("nome")
+const distance = document.getElementById("km")
+const Età = document.getElementById("età")
+const buttonGenera = document.getElementById("genera")
+const buttonAnnulla = document.getElementById("annulla")
+const prezzoPerKm = 0.21;
+
+// Calcolo del biglietto
